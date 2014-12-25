f183e775576887556f72a251a60f8c76df229ae0
Thomas Herrmann <mail@thoherr.de>
2014-12-25 22:13:55 +0100

    add tests with access to underlying actor

--- src/test/scala/ws/fortytwo/experimental/akka/philosophers/PhilosopherSpec.scala
+++ src/test/scala/ws/fortytwo/experimental/akka/philosophers/PhilosopherSpec.scala
@@ -10,12 +10,43 @@ class PhilosopherSpec extends TestKit(ActorSystem("ChopstickSpec")) with Implici
 
   override def afterAll() { system.shutdown() }
 
+  def actor() = {
+    val a = TestActorRef[Philosopher](Props(classOf[Philosopher], testActor, testActor))
+    (a, a.underlyingActor)
+  }
+
   "Philosopher" should {
     "try to take chopsticks after thinking for a while" in {
-      val p = TestActorRef(Props(classOf[Philosopher], testActor, testActor))
+      val (p, _) = actor()
       p ! Think
       expectMsg(5.seconds, Chopstick.Take)
       expectMsg(Chopstick.Take)
     }
+
+    "try to take chopsticks when getting hungry" in {
+      val (p, real) = actor()
+      real.context.become(real.thinking)
+      p ! Eat
+      expectMsg(Chopstick.Take)
+      expectMsg(Chopstick.Take)
+    }
+
+    "put back both chopsticks when getting a denial of the first chopstick" in {
+      val (p, real) = actor()
+      real.context.become(real.hungry)
+      p ! Chopstick.ChopstickInUse
+      expectMsg(Chopstick.Put)
+      expectMsg(Chopstick.Put)
+    }
+
+    "put back both chopsticks when getting a denial of the second chopstick" in {
+      val (p, real) = actor()
+      real.context.become(real.waitingForOtherChopstick)
+      p ! Chopstick.ChopstickInUse
+      expectMsg(Chopstick.Put)
+      expectMsg(Chopstick.Put)
+    }
+
   }
+
 }
